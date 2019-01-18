ccbc1f197aa8798c6ac28ba2887c2e279eccf3c7
Meria Thomas <josemeria93@gmail.com>
2019-01-18 15:19:55 -0800

    add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 function Header(){
   return (
     <div>
       <style jsx>{`
-      color:white;
-      text-align: center;
-      margin-top: 8%;
-      .jumbotron {
-        background-color: rgba(235,28,35, 0.4);
-        font-family: 'Damion', cursive;
-        font-weight: bold;
-        font-size:70px;
-        color: #045905;
-        text-shadow: 2px 2px 8px white;
+      .bg{
+        background-color: #d7dae0;
+        height: 50px;
+        padding-left: 100px;
+        padding-top: 0.7em;
       }
-      Link {
 
-      }
       `} </style>
-      <h1 className="jumbotron">Avery's Organics</h1>
-      <Link className='btn btn-secondary' to='/'>Home</Link> | <Link className='btn btn-secondary'to='/months'>Seasonal Availability</Link>
+    <p className="bg">Assemble Books</p>
     </div>
   );
 }
